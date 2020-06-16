d96784445179741538f7ae897f3a8ff6f42264be
R. Pablo Gomes <rpablo.gdr@gmail.com>
2020-06-16 16:42:48 -0300

    Update imc.js
    This code calculates the BMI and displays the weight condition on console.

--- imc.js
+++ imc.js
@@ -5,8 +5,8 @@ const altura = 1.88;
 const imc = peso / (altura * altura);
 
     if(imc > 30){
-    console.log(`${nome} você está acima do peso`)
+    console.log(`${nome}, você está acima do peso`)
 }
     else{
-        console.log(`${nome} você não está acima do peso`)
+        console.log(`${nome}, você não está acima do peso`)
 }
