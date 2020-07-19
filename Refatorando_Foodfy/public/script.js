c7e2a979c00fd9ace54f6a4d196d626fa1618a69
R. Pablo <rpablo.gdr@gmail.com>
2020-07-18 21:25:12 -0300

    working on visibility of sections on receipt

--- Refatorando_Foodfy/public/script.js
+++ Refatorando_Foodfy/public/script.js
@@ -1,16 +1,32 @@
-// const  indexOf  = require("../data.js")
-
-// const { indexOf } = require("../data")
-
-// const { indexOf } = require("../data")
-
 for (let card of document.getElementsByClassName("card")) {
     card.addEventListener("click", function () {
         const foodIndex = card.getAttribute("id")
         window.location.href = `/receipts/${foodIndex}`
     })
-} 
+}
+
 // document.querySelector("#x").addEventListener("click", function () {
 //     document.querySelector("#modal").setAttribute("hidden", "")
 //     document.getElementById("iframe").setAttribute("src", "")
 // })
+
+
+
+document.querySelector("#hiddenFunctionOfIngredients")
+    .addEventListener("click", function () {
+        document.querySelector(".hiddenFunctionOfIngredientsTEXT").toggleAttribute("hidden")
+     
+})
+
+document.querySelector(".hiddenFunctionOfSteps")
+    .addEventListener("click", function () {
+        document.querySelector("#hiddenFunctionOfStepsTEXT").toggleAttribute("hidden")
+        document.querySelector("hiddenFunctionOfSteps")
+        // document.getElementsByClassName("hiddenFunctionOfSteps").toggleAttribute("hidden")
+        // document.querySelector("#hiddenFunctionOfStepsTEXT").toggleAttribute("hidden")
+})
+
+document.getElementById("hiddenFunctionOfInformations")
+    .addEventListener("click", function () {
+        document.getElementById("hiddenFunctionOfInformationsTEXT").toggleAttribute("hidden")
+})
