962e691b8196fadaa80c7d66d8e27600e5cb1625
R. Pablo <rpablo.gdr@gmail.com>
2020-07-28 21:56:21 -0300

    working on CSS of layout

---
+++ Academia/routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const express = require("express")
+const routes = express.Router()
+
+routes.get("/", function (req, res){
+    return res.render("layout")
+})
+
+module.exports = routes
--- Academia/server.js
+++ Academia/server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const nunjucks = require("nunjucks")
+const routes = require("./routes")
 
 const server = express()
-const data = require("./data")
-const courses = require("./data").courses
+
 
 server.use(express.static("public"))
+server.use(routes)
 
 server.set("view engine", "njk")
 
@@ -20,45 +21,3 @@ server.listen(5000, function () {
 })
 
 // routes
-
-server.get("/", function (req, res) {
-    return res.render("layout", { menu: data.menu })
-})
-
-server.get("/inicio", function (req, res) {
-    return res.render("/")
-})
-
-server.get("/about", function (req, res) {
-    return res.render("rocketseat", { about: data.about, menu: data.menu })
-})
-
-server.get("/course", function (req, res) {
-    const videoId = req.query.id
-
-    const video = courses.find(function (video) {
-        if (videoId == video.id) {
-            return true
-        }
-    })
-
-    if (!video) {
-        return res.render("not-found")
-    }
-
-    res.render("video", { id: video.id })
-    // res.render( "video", { videoId }, { menu: data.menu })
-})
-
-// server.get("/courses/:id", function(req, res) {
-//     const id = req.params.id;
-//     return res.render("video", {id});
-//   });
-
-server.get("/content", function (req, res) {
-    return res.render("content", { courses: data.courses, menu: data.menu })
-})
-
-server.use(function (req, res) {
-    res.status(404).render("not-found",);
-})
