260e6b9effb34d8f52f21d7797ad859c48075ed1
R. Pablo <rpablo.gdr@gmail.com>
2020-12-28 21:51:11 -0300

    working on fields

--- Modelando_dados/src/routes.js
+++ Modelando_dados/src/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routes = express.Router();
+const ProductsController = require("./app/controllers/ProductsController")
 
 
 //layout
@@ -7,5 +8,10 @@ routes.get("/", function(req, res) {
   return res.render("layout");
 });
 
+routes.get("/products/create", ProductsController.create);
+
+routes.get("/ads/create", function(req, res) {
+  return res.redirect("/products/create");
+});
 
 module.exports = routes;
