3ca1e1076aeb4dc10b7ac05868b3ca2269226530
R. Pablo <rpablo.gdr@gmail.com>
2021-01-11 19:33:22 -0300

    working on puting and deleting

--- Modelando_dados/src/app/controllers/ProductsController.js
+++ Modelando_dados/src/app/controllers/ProductsController.js
@@ -1,37 +1,84 @@
-const Category = require("../models/Category")
-const Product = require("../models/Product")
+const { formatPrice } = require("../../lib/utils");
+
+const Category = require("../models/Category");
+const Product = require("../models/Product");
 
 module.exports = {
-    create(req, res) {
-        //Pegar Categorias
-        Category.All()
-        .then(function(results){
-            const categories = results.rows 
+  create(req, res) {
+    //Pegar Categorias
+    Category.All()
+      .then(function (results) {
+        const categories = results.rows;
 
-            return res.render('products/create.njk', { categories})
-        }).catch(function(err){
-            throw new Error(err)
-        })
-    },
+        return res.render("products/create.njk", { categories });
+      })
+      .catch(function (err) {
+        throw new Error(err);
+      });
+  },
 
-    async post(req, res) {
-        const keys = Object.keys(req.body);
-    
-        //validation
-        for (key of keys) {
-          if (req.body[key] == "") {
-            return res.send("Fill all the fields");
-          }
-        }
+  async post(req, res) {
+    const keys = Object.keys(req.body);
 
-        let results = await Product.create(req.body)
-
-        const productId = results.rows[0].id
-
-        results = await Category.All()
-        const categories = results.rows[0]
-
-        return res.render("products/create.njk", { productId, categories })
-
-      } 
+    //validation
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Fill all the fields");
+      }
     }
+
+    req.body.price = req.body.price.replace(/\D/g, "");
+
+    let results = await Product.create(req.body);
+    const productId = results.rows[0].id;
+
+    return res.redirect(`products/${productId}`);
+  },
+
+  async edit(req, res) {
+    let results = await Product.find(req.params.id);
+    const product = results.rows[0];
+
+    if (!product) return res.send("Product not found ");
+
+    product.old_price = formatPrice(product.old_price);
+    product.price = formatPrice(product.price);
+
+    results = await Category.All();
+    const categories = results.rows;
+
+    return res.render("products/edit.njk", { product, categories });
+  },
+
+  async put (req, res) {
+
+    const keys = Object.keys(req.body)
+
+    console.log(req.body)
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Fill all the fields");
+      }
+    }
+    req.body.price = req.body.price.replace(/\D/g, "");
+
+    if(req.body.old_price != req.body.price){
+      const oldProduct = await Product.find(req.body.id)
+      req.body.old_price = oldProduct.rows[0].price
+    }
+
+    await Product.update(req.body)
+
+    return res.redirect(`/products/${req.body.id}/edit`)
+  },
+  
+  async delete(req, res){
+
+    console.log(req)
+
+    await Product.delete(req.body.id)
+
+    return res.redirect(`/`)
+  }
+};
--- Modelando_dados/src/routes.js
+++ Modelando_dados/src/routes.js
@@ -9,10 +9,14 @@ routes.get("/", function(req, res) {
 });
 
 routes.get("/products/create", ProductsController.create);
+routes.get("/products/:id/edit", ProductsController.edit);
 routes.post("/products", ProductsController.post)
+routes.put("/products", ProductsController.put)
+routes.delete("/products", ProductsController.delete)
+
 
 // Alias
-routes.get("/ads/create", function(req, res) {
+routes.get("/ads/create", function(req, res) { 
   return res.redirect("/products/create");
 });
 
